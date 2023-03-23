b997900a53f599d91eea12c82caa02de057f0bc1
Семен Черняев <cemenchern001@gmail.com>
2023-03-23 20:12:22 +0300

    Update serializers.py

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -2,9 +2,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.shortcuts import get_object_or_404
 from rest_framework import exceptions, serializers
 from rest_framework_simplejwt.tokens import AccessToken
-
-
-from reviews.models import Category, Comment, Genre, Title, Review
+from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import User
 
 USERNAME_REGEX = r'^(?!me\Z)^[\w.@+-]+\Z'
